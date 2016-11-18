5afb4d27fc5f0f3c10ddb696b7e717156bfae1dd
HelloRichard <richardhlmavs214@yahoo.com>
2016-11-18 22:04:21 +0000

    finished variable exercises

---
+++ prep_exercise/name.rb
@@ -0,0 +1,12 @@
+#Exercise 1 & 4
+puts "Enter your first name:"
+first_name = gets.chomp
+puts "Enter your last name:"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + last_name
+
+#3
+# 10.times do |n|
+#   puts name
+# end
+
--- prep_exercise/solution.rb
+++ prep_exercise/solution.rb
@@ -1,4 +1,4 @@
-#Exercise one
+#Basic Exercise one
 a = "John " + "Doe"
 # puts a
 
@@ -32,12 +32,16 @@ years = [1975, 2004, 2013, 2001, 1981]
 # puts years[4]
 
 #Five
-puts 5 * 4 * 3 * 2 * 1
-puts 6 * 5 * 4 * 3 * 2 * 1
-puts 7 * 6 * 5 * 4 * 3 * 2 * 1
-puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+# puts 5 * 4 * 3 * 2 * 1
+# puts 6 * 5 * 4 * 3 * 2 * 1
+# puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+# puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
 
 #Six
-puts 7.77*7.77
-puts 8.88*8.88
-puts 9.99*9.99
+# puts 7.77*7.77
+# puts 8.88*8.88
+# puts 9.99*9.99
+
+
+
+
