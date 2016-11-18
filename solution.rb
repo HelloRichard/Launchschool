37730d0d3fde59fdcd06e40cbbfac04fb1a04b12
HelloRichard <richardhlmavs214@yahoo.com>
2016-11-18 06:27:02 +0000

    finished the first exercise

---
+++ solution.rb
@@ -0,0 +1,43 @@
+#Exercise one
+a = "John " + "Doe"
+# puts a
+
+#Two
+thousands = 5094/1000
+hundreds = 5094%1000/100
+tens = 5094%1000%100/10
+ones = 5094%1000%100%10
+
+# puts thousands
+# puts hundreds
+# puts tens
+# puts ones
+
+#Three
+movies = {jaws: 1975, anchorman: 2004, man_of_steel: 2013, a_beautiful_mind: 2001, the_evil_dead: 1981}
+
+# puts movies[:jaw]
+# puts movies[:anchorman]
+# puts movies[:man_of_steel]
+# puts movies[:a_beautiful_mind]
+# puts movies[:the_evil_dead]
+
+#Four
+years = [1975, 2004, 2013, 2001, 1981]
+
+# puts years[0]
+# puts years[1]
+# puts years[2]
+# puts years[3]
+# puts years[4]
+
+#Five
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#Six
+puts 7.77*7.77
+puts 8.88*8.88
+puts 9.99*9.99
