6cc39464a098f983fd824da662d3cae2439c235c
HelloRichard <richardhlmavs214@yahoo.com>
2016-11-22 06:31:06 +0000

    added exercise for methods

---
+++ prep_exercise/methods.rb
@@ -0,0 +1,21 @@
+#Methods Exercise 1
+def greeting(name)
+  "Hello there #{name}!"
+end
+
+puts greeting("John")
+
+#2
+def multiply(a, b)
+  a * b
+end
+
+puts multiply(9, 9)
+
+#3
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
